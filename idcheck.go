9a7a6fdc4c65606726a07a487f1e84126a79ed16
Will Roden <willabides@github.com>
2018-07-08 16:12:02 -0500

    initial commit

---
+++ idcheck.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"io"
+	"crypto/rand"
+	"encoding/base64"
+)
+
+var hashTable = func() [256]uint8 {
+	var vals [256]uint8
+	for n, _ := range vals {
+		vals[n] = uint8(n)
+	}
+	for _, i := range []int{2, 3, 5, 7, 11} {
+		for n, _ := range vals {
+			vals[n], vals[n/i] = vals[n/i], vals[n]
+		}
+	}
+	return vals
+}()
+
+func hash(data []byte) (hsh uint8) {
+	for _, d := range data {
+		hsh = hashTable[hsh^d]
+	}
+	return
+}
+
+type Service struct {
+	IDLength int
+	idReader io.Reader
+}
+
+type ServiceOpt func(*Service)
+
+func WithReader(reader io.Reader) ServiceOpt {
+	return func(idSvc *Service) {
+		idSvc.idReader = reader
+	}
+}
+
+func WithIDLength(idLength int) ServiceOpt {
+	return func(svc *Service) {
+		svc.IDLength = idLength
+	}
+}
+
+func NewService(opts ...ServiceOpt) *Service {
+	svc := &Service{
+		idReader: rand.Reader,
+		IDLength: 16,
+	}
+	for _, opt := range opts {
+		opt(svc)
+	}
+	return svc
+}
+
+func NewID() (ID, error) {
+	return NewService().NewID()
+}
+func (svc *Service) NewID() (ID, error) {
+	idBytes := make([]byte, svc.IDLength-1)
+	_, err := svc.idReader.Read(idBytes)
+	if err != nil {
+		return nil, err
+	}
+	hashByte := byte(hash(idBytes))
+	idBytes = append(idBytes, hashByte)
+	return ID(idBytes), nil
+}
+
+type ID []byte
+
+func ValidID(id ID) bool {
+	return NewService().ValidID(id)
+}
+func (svc *Service) ValidID(id ID) bool {
+	if len(id) != svc.IDLength {
+		return false
+	}
+	hsh := hash([]byte(id)[:svc.IDLength-1])
+	return hsh == []byte(id)[svc.IDLength-1]
+}
+
+func (id ID) Base64() string {
+	return base64.RawURLEncoding.EncodeToString(id)
+}
+
+func FromBase64(str string) (ID, error) {
+	b, err := base64.RawURLEncoding.DecodeString(str)
+	if err != nil {
+		return nil, err
+	}
+	return ID(b), nil
+}
---
+++ idcheck_test.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"testing"
+	"math"
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	)
+
+func TestHashTable(t *testing.T) {
+	t.Run("contains all uint8 values", func(t *testing.T) {
+		for i := 0; i <= math.MaxUint8; i++ {
+			found := false
+			for _, n := range hashTable {
+				if n == uint8(i) {
+					found = true
+					break
+				}
+			}
+			if !found {
+				t.Logf("missing value: %d", i)
+				t.Fail()
+			}
+		}
+	})
+
+	t.Run("is sufficiently shuffled", func(t *testing.T) {
+		for i,v := range hashTable{
+			if uint8(i) == v {
+				t.Logf("table position and value coincide at %d", i)
+			}
+		}
+	})
+}
+
+func TestService_NewID_NewID(t *testing.T) {
+	t.Run("hash byte matched known good value", func(t *testing.T) {
+		bts := make([]byte, 15)
+		rdr := bytes.NewReader(bts)
+		svc := NewService(WithReader(rdr))
+		id, err := svc.NewID()
+		assert.Nil(t, err)
+		assert.Equal(t, bts, []byte(id)[:15])
+		assert.Equal(t, uint8(0xec), []byte(id)[15])
+	})
+}
+
+func TestFromBase64(t *testing.T) {
+	t.Run("empty string", func(t *testing.T) {
+		id, err := FromBase64("")
+		assert.Nil(t, err)
+		assert.Equal(t, make(ID, 0), id)
+	})
+
+	t.Run("straight a", func(t *testing.T) {
+		id, err := FromBase64("AAAAAAAAAAAAAAAAAAAAAA")
+		assert.Nil(t, err)
+		assert.Equal(t, make(ID, 16), id)
+	})
+}
+
+func TestID_Base64(t *testing.T) {
+	assert.Equal(t, "AAAAAAAAAAAAAAAAAAAAAA", make(ID, 16).Base64())
+	id, err := NewID()
+	assert.Nil(t, err)
+	idStr := id.Base64()
+	id2, err := FromBase64(idStr)
+	assert.Nil(t, err)
+	assert.Equal(t, id, id2)
+}
+
+func TestService_ValidID(t *testing.T) {
+	t.Run("returns true for a valid ID", func(t *testing.T) {
+		idBytes := make([]byte, 16)
+		idBytes[15] = 0xec
+		id := ID(idBytes)
+		assert.True(t, ValidID(id))
+	})
+
+	t.Run("returns false when hash doesn't match", func(t *testing.T) {
+		idBytes := make([]byte, 16)
+		idBytes[15] = 0x01
+		id := ID(idBytes)
+		assert.False(t, ValidID(id))
+	})
+
+	t.Run("returns false for empty id", func(t *testing.T) {
+		var id ID
+		assert.False(t, ValidID(id))
+	})
+
+	t.Run("returns false when id is too long", func(t *testing.T) {
+		idBytes := make([]byte, 16)
+		idBytes[15] = 0xec
+		id := ID(idBytes)
+		assert.False(t, NewService(WithIDLength(15)).ValidID(id))	})
+
+
+	t.Run("returns false when id is too short", func(t *testing.T) {
+		idBytes := make([]byte, 16)
+		idBytes[15] = 0xec
+		id := ID(idBytes)
+		assert.False(t, NewService(WithIDLength(17)).ValidID(id))	})
+}
